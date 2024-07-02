a1d658777730f99b0defb1f15f01ee9a5fc60dc0
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-07-02 10:22:39 -0700

    feat: codegen error reporting (#44)
    * feat: codegen error reporting
    
    * chore: typos
    
    * chore: formatting

--- ecsact/codegen/plugin.h
+++ ecsact/codegen/plugin.h
@@ -36,6 +36,48 @@ typedef void (*ecsact_codegen_write_fn_t)( //
 	int32_t     str_len
 );
 
+typedef enum ecsact_codegen_report_type {
+	/**
+	 * Informational report. Mostly used for debugging. May or may not be shown to
+	 * user.
+	 */
+	ECSACT_CODEGEN_REPORT_INFO,
+
+	/**
+	 * Warning. May or may not be shown to user.
+	 */
+	ECSACT_CODEGEN_REPORT_WARNING,
+
+	/**
+	 * An error has occurred, but can still continue running. Will be shown to
+	 * user when possible.
+	 */
+	ECSACT_CODEGEN_REPORT_ERROR,
+
+	/**
+	 * An error has occurred and plugin cannot continue running. Will be shown to
+	 * user when possible.
+	 */
+	ECSACT_CODEGEN_REPORT_FATAL,
+} ecsact_codegen_report_message_type;
+
+/**
+ * Message passed to this function is reported to the codegen host and may be
+ * shown to the user. Characters that go beyond @p msg_len are not read. Some
+ * report types may hault the codegen process. @see ecsact_codegen_report_type
+ *
+ * @NOTE: it is _NOT_ assumed that @p msg is null-terminated, you must set
+ * @p msg_len properly.
+ *
+ * @param msg - array of characters of length @p msg_len
+ * @param msg_len - length of array of characters @p msg
+ */
+typedef void (*ecsact_codegen_report_fn_t)( //
+	ecsact_codegen_report_type report_type,
+	const char*                msg,
+	int32_t                    msg_len
+);
+
 ECSACT_CODEGEN_PLUGIN_API const char* ecsact_codegen_plugin_name();
 
 /**
@@ -50,8 +92,9 @@ ECSACT_CODEGEN_PLUGIN_API const char* ecsact_codegen_plugin_name();
  *        output
  */
 ECSACT_CODEGEN_PLUGIN_API void ecsact_codegen_plugin( //
-	ecsact_package_id         package_id,
-	ecsact_codegen_write_fn_t write_fn
+	ecsact_package_id          package_id,
+	ecsact_codegen_write_fn_t  write_fn,
+	ecsact_codegen_report_fn_t report_fn
 );
 
 #endif // ECSACT_CODEGEN_PLUGIN_H
